7d9bc52f2fcd8beb35e2f6f5e15a54e385e2d494
Claudia <claudiasrib@gmail.com>
2025-06-13 14:16:06 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/neoCamp/partidaFutebol/PartidaFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.neoCamp.partidaFutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidaFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidaFutebolApplication.class, args);
+	}
+
+}
